7429d6b91ed17487bd22686f2962a947e16b9757
Thierry Treyer <ttreyer@fb.com>
2019-11-08 22:24:23 -0800

    Add BallLauncher script

---
+++ Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncher : MonoBehaviour {
+    private Rigidbody _rbody;
+    public float _force;
+
+    private void Awake() {
+        _rbody = GetComponent<Rigidbody>();
+    }
+
+    void Launch(float force) {
+        _force = force;
+    }
+
+    private void FixedUpdate() {
+        _rbody.AddForce(transform.parent.forward * _force);
+        _force = 0f;
+    }
+}
