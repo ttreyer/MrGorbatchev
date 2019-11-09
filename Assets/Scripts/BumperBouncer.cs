a3328f65535db0d9adbb65e459995ca3d0fba161
Thierry Treyer <ttreyer@fb.com>
2019-11-09 13:02:18 -0800

    Add Bumper prefab

---
+++ Assets/Scripts/BumperBouncer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperBouncer : MonoBehaviour {
+    public float _bumperForce = 40f;
+
+    private void OnCollisionEnter(Collision collision) {
+        ContactPoint contact = collision.GetContact(0);
+        Rigidbody ballRbody = collision.rigidbody;
+
+        ballRbody.AddForce(-contact.normal * _bumperForce, ForceMode.VelocityChange);
+    }
+}
