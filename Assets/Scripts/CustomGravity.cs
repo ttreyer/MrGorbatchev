32bebc1189eb48ade7ed8bf849e7d969fcaa82eb
Thierry Treyer <ttreyer@fb.com>
2019-11-08 22:50:37 -0800

    Custom Gravity + Flipper base

---
+++ Assets/Scripts/CustomGravity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomGravity : MonoBehaviour {
+    public float _gravityMultiplier = 1f;
+
+    private Rigidbody _rbody;
+
+    private void Awake() {
+        _rbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        _rbody.AddForce(Physics.gravity * _rbody.mass * (_gravityMultiplier - 1f));
+    }
+}
