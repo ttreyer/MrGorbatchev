15f1cbd7071247c5dc401874546260c64c6f6d41
Thierry Treyer <ttreyer@fb.com>
2019-11-10 12:55:00 -0800

    Setup BallManager

---
+++ Assets/Scripts/BallManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour {
+    public GameObject _ballPrefab;
+    public LayerMask _ballLayer;
+    public Transform _ballSpawn;
+
+    private HashSet<GameObject> _balls;
+
+    private bool SpawnEmpty() {
+        return !Physics.CheckSphere(_ballSpawn.position, 1f, _ballLayer);
+    }
+
+    public void SpawnBall() {
+        if (SpawnEmpty())
+            _balls.Add(Instantiate(_ballPrefab, _ballSpawn));
+    }
+}
