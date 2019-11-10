25b77b2c1aa5e8eae13d78ea527d54a5269a4d7a
Thierry Treyer <ttreyer@fb.com>
2019-11-09 21:11:15 -0800

    Time based spawn

--- Assets/Scripts/TetrisSpawner.cs
+++ Assets/Scripts/TetrisSpawner.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class TetrisSpawner : MonoBehaviour {
     public GameObject[] _tetraminos;
+    public float _spawnInterval = 3.5f;
 
     public int _tetraminoIndex;
 
+    private float _spawnTimer;
+
     public void Spawn() {
+        _spawnTimer = Time.time + _spawnInterval;
         Instantiate(_tetraminos[_tetraminoIndex], transform);
         _tetraminoIndex = (_tetraminoIndex + 1) % _tetraminos.Length;
+    }
+
+    private void Update() {
+        if (Time.time > _spawnTimer)
+            Spawn();
     }
 }
