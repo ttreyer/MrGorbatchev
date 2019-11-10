846c451d46d0b55bdf5671d964b740787f062359
Megan Taylor <megamijadeheart@gmail.com>
2019-11-10 11:14:57 -0800

    UI Music Manager
    Music manager script is attatched to a pervasice game object so it will exist in every scene.  It can change the background music or play sound effects.  the sound clips are stored in an array in the scrip

---
+++ Assets/UI/UI Scripts/MusicManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance;
+
+    [SerializeField] private AudioClip[] musicClips;
+    [SerializeField] private AudioClip[] soundEffects;
+    private AudioSource myAudioSource;
+    [SerializeField] private bool musicIsPlaying = false;
+
+    [SerializeField] private int currentClip;
+
+
+    private void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    void Start()
+    {
+        myAudioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnSoundEffect(Vector3 location, int clipIndex)
+    {
+        //check if the clip can exist in the array
+        if (clipIndex > soundEffects.Length)
+        {
+            Debug.Log("Clip " + clipIndex + " does not exist in the soundEffects Array.");
+            return;
+        }
+
+        AudioClip clip = soundEffects[clipIndex];
+        AudioSource.PlayClipAtPoint(clip, location);
+    }
+
+    public void PlayMusic(int clipIndex)
+    {
+        //check if the clip can exist in the array
+        if (clipIndex > musicClips.Length)
+        { Debug.Log("Clip " + clipIndex + " does not exist in the musicClips Array.");
+            return;
+        }
+
+        if (currentClip == clipIndex && musicIsPlaying) return;
+
+        else if(musicIsPlaying)
+            { StopTheMusic(); }
+
+        //assign and start the new clip
+        currentClip = clipIndex;
+        myAudioSource.clip = musicClips[currentClip];
+        myAudioSource.loop = true;
+        myAudioSource.Play();
+        musicIsPlaying = true;
+    }
+
+    public void StopTheMusic()
+    {
+        myAudioSource.Stop();
+        musicIsPlaying = false;
+    }
+}
