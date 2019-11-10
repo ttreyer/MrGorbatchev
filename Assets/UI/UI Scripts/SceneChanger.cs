53156252b3cd87398e19af55d6b43ecf2ea21594
Megan Taylor <megamijadeheart@gmail.com>
2019-11-10 13:10:36 -0800

    UI Start Menu
    Working start menu with music, Start button transitions to the next scene in the build settings.

---
+++ Assets/UI/UI Scripts/SceneChanger.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public static SceneChanger instance;
+    private int currentSceneIndex;
+
+
+    private void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log("The current Scene is " + currentSceneIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGivenScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadGivenScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadNextScene()
+    {
+        Debug.Log("The next Scene is " + currentSceneIndex++);
+        SceneManager.LoadScene(currentSceneIndex++);
+    }
+}
