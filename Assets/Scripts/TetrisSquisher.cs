bca1bac7584e768d2aca257041d544bd68031fe9
Thierry Treyer <ttreyer@fb.com>
2019-11-10 11:35:32 -0800

    Squish piece based on the rotation

---
+++ Assets/Scripts/TetrisSquisher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisSquisher : MonoBehaviour {
+    public float _squishAmount = 0.05f;
+
+    // Squish the shape to be thinner on the X-axis, independently of the rotation
+    private void Awake() {
+        float rot = transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
+        float rx = Mathf.Abs(Mathf.Cos(rot));
+        float ry = Mathf.Abs(Mathf.Sin(rot));
+
+        foreach (var col in GetComponents<BoxCollider>()) {
+            col.size = new Vector3(
+                col.size.x - rx * _squishAmount,
+                col.size.y - ry * _squishAmount,
+                col.size.z);
+        }
+    }
+}
