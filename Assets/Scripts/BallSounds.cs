d253bf69cf5622eac3174af4fd1b0e3299741fdf
Thierry Treyer <ttreyer@fb.com>
2019-11-10 15:53:08 -0800

    Add sound on ball hit

---
+++ Assets/Scripts/BallSounds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSounds : MonoBehaviour {
+    public AudioSource _ballHitSource;
+
+    public AudioClip[] _hitSounds;
+
+    private void OnCollisionEnter(Collision collision) {
+        int hitIndex = Random.Range(0, _hitSounds.Length);
+        _ballHitSource.pitch = Random.Range(0.8f, 1.5f);
+        _ballHitSource.PlayOneShot(_hitSounds[hitIndex]);
+    }
+}
