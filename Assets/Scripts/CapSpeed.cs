1fdc4614302e75407ef5388e66737f5356f31428
Thierry Treyer <ttreyer@fb.com>
2019-11-10 13:56:53 -0800

    Add a speed limit to the ball

---
+++ Assets/Scripts/CapSpeed.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapSpeed : MonoBehaviour {
+    public float _maxSpeed = 50f;
+
+    private Rigidbody _rbody;
+
+    private void Awake() {
+        _rbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        if (_rbody.velocity.sqrMagnitude > (_maxSpeed * _maxSpeed))
+            _rbody.velocity = _rbody.velocity.normalized * _maxSpeed;
+    }
+}
