a6ac1e6b1716a46302ec3994cbb1be984897bc74
Thierry Treyer <ttreyer@fb.com>
2019-11-09 20:19:14 -0800

    Simple spawner

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class InputManager : MonoBehaviour {
     public FlipperRotator _leftFlipper, _rightFlipper;
     public BallLauncher _ball;
+    public TetrisSpawner _tetrisSpawner;
 
     public float _ballLaunchForce = 2500f;
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Space))
             _ball.Launch(_ballLaunchForce);
+        if (Input.GetKeyDown(KeyCode.Return))
+            _tetrisSpawner.Spawn();
 
         _leftFlipper.FlipToggle(Input.GetKey(KeyCode.LeftArrow));
         _rightFlipper.FlipToggle(Input.GetKey(KeyCode.RightArrow));
---
+++ Assets/Scripts/TetrisSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisSpawner : MonoBehaviour {
+    public GameObject[] _tetraminos;
+
+    public int _tetraminoIndex;
+
+    public void Spawn() {
+        Instantiate(_tetraminos[_tetraminoIndex], transform);
+        _tetraminoIndex = (_tetraminoIndex + 1) % _tetraminos.Length;
+    }
+}
