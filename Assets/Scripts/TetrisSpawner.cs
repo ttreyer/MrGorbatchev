1e2d9f2976c781459b6312147b97f5e6e25fdf77
Thierry Treyer <ttreyer@fb.com>
2019-11-10 11:35:53 -0800

    Prevent duplicate pieces+Refactor

--- Assets/Scripts/TetrisSpawner.cs
+++ Assets/Scripts/TetrisSpawner.cs
@@ -8,28 +8,52 @@ public class TetrisSpawner : MonoBehaviour {
 
     private float _spawnTimer;
     private BoxCollider _spawnBox;
+    private int _lastTetromino;
 
     private void Awake() {
         _spawnBox = GetComponent<BoxCollider>();
+    }
+
+    private GameObject RandomTetromino() {
+        // Select a piece different from the last one
+        int spawnIndex = _lastTetromino;
+        while (spawnIndex == _lastTetromino)
+            spawnIndex = Random.Range(0, _tetraminos.Length);
+        _lastTetromino = spawnIndex;
+
+        return _tetraminos[spawnIndex];
+    }
+
+    private Vector3 RandomPosition() {
+        // Select random pos on X-axis, aligned on grid with the Floor()
+        float spawnMin = transform.position.x - (_spawnBox.size.x / 2f);
+        float spawnMax = transform.position.x + (_spawnBox.size.x / 2f) + 1f;
+        float spawnX = Mathf.Floor(Random.Range(spawnMin, spawnMax));
+
+        return new Vector3(spawnX, transform.position.y, transform.position.z);
+    }
+
+    private Quaternion RandomRotation() {
+        int rotIndex = Random.Range(0, 4);
+        float rotDeg = rotIndex * 90f;
+        return transform.rotation * Quaternion.Euler(0f, 0f, rotDeg);
     }
 
     public void Spawn() {
         // Reset timer for next tetramino
         _spawnTimer = Time.time + _spawnInterval;
 
-        // Select random pos on X-axis, aligned on grid with the Floor()
-        float spawnMin = transform.position.x - (_spawnBox.size.x / 2f);
-        float spawnMax = transform.position.x + (_spawnBox.size.x / 2f) + 1f;
-        float spawnX = Mathf.Floor(Random.Range(spawnMin, spawnMax));
+        // Select a random position
+        Vector3 spawnPos = RandomPosition();
 
-        Vector3 spawnPos = new Vector3(spawnX, transform.position.y, transform.position.z);
+        // Select a random rotation
+        Quaternion rotation = RandomRotation();
 
-        // Select the piece randomly
-        int spawnIndex = Random.Range(0, _tetraminos.Length);
-        GameObject spawnTetramino = _tetraminos[spawnIndex];
+        // Select a random tetromino
+        GameObject spawnTetramino = RandomTetromino();
 
         // Instantiate the new piece
-        Instantiate(spawnTetramino, spawnPos, transform.rotation);
+        Instantiate(spawnTetramino, spawnPos, rotation);
     }
 
     private void Update() {
