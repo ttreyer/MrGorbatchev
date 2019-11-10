ea79012d6d84888c3834bda19dc5a16009794c6f
Megan Taylor <megamijadeheart@gmail.com>
2019-11-09 18:09:21 -0800

    UI Scrolling Headlines
    Newsreel script will scroll headlines in a loop.  Scorekeeper can change headlines when milestones are reached.
    empty array to be filled with headlines.

--- Assets/UI/UI Scripts/NewsReel.cs
+++ Assets/UI/UI Scripts/NewsReel.cs
@@ -5,26 +5,142 @@ using UnityEngine.UI;
 
 public class NewsReel : MonoBehaviour
 {
-
+    public string[] headlines;
     [SerializeField] private Text newsText;
     [SerializeField] private string news;
+    RectTransform textRextTransform;
+    private float charLengthInPx = 43f;
+    private float startPos = 660f;
+    private Vector2 textStartPosition;
+    private float totalAdvanced = 0f;
+
+    [SerializeField] private bool scrollright = false;
+    [SerializeField] private bool flash = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        textRextTransform = newsText.GetComponent<RectTransform>();
         news = null;
+
+        UpdateNews("Hello World.", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //ForTesting
-        UpdateNews("Hello World");
+        if (scrollright && !flash)
+        {
+            if (totalAdvanced == 0f)
+           {
+                StartCoroutine("ScrollFromRight");
+           }
+        }
+        else if (scrollright && flash)
+        {
+            if (totalAdvanced == 0f)
+            {
+                StartCoroutine("ScrollFromRightThenFlash");
+            }
+        }
     }
 
     //update the score and score display durring gameplay
-    public void UpdateNews(string newHeadline)
+    public void UpdateNews(string newHeadline, bool scroll)
     {
+        scrollright = scroll;
         news = newHeadline;
         newsText.text = news;
+
+        if(scroll)
+        {
+            textStartPosition = new Vector2(textRextTransform.anchoredPosition.x, -93.8f);
+
+            Debug.Log("Text Start Position is" + textStartPosition.x);
+        }
+        else
+        {
+            textStartPosition = new Vector2(textRextTransform.anchoredPosition.x,
+                                            textRextTransform.anchoredPosition.y);
+        }
+
+    }
+
+    public void UpdateNews(int headlineIndex, bool scroll)
+    {
+        scrollright = scroll;
+        news = headlines[headlineIndex];
+        newsText.text = news;
+
+        if (scroll)
+        {
+            textStartPosition = new Vector2(textRextTransform.anchoredPosition.x, -93.8f);
+
+            Debug.Log("Text Start Position is" + textStartPosition.x);
+        }
+        else
+        {
+            textStartPosition = new Vector2(textRextTransform.anchoredPosition.x,
+                                            textRextTransform.anchoredPosition.y);
+        }
+
+    }
+
+    private IEnumerator ScrollFromRight()
+    {
+        totalAdvanced = 1;
+        float advanceBy = 5f;
+        float textLength = TextLength();
+
+        textRextTransform.anchoredPosition = textStartPosition;
+
+        while (totalAdvanced <= (textLength+startPos))
+        {
+            yield return new WaitForSecondsRealtime(.1f);
+            //advance text field by advanceBy in the X
+            textRextTransform.anchoredPosition = new Vector3 (textStartPosition.x -totalAdvanced,
+                                                                textStartPosition.y
+                                                                );
+            totalAdvanced += advanceBy;
+
+            Debug.Log("total advanced is " + totalAdvanced);
+        }
+
+        totalAdvanced = 0;
+        yield break;
+    }
+
+
+    private IEnumerator ScrollFromRightThenFlash()
+    {
+        totalAdvanced = 1;
+        float advanceBy = 5f;
+        float textLength = TextLength();
+
+        textRextTransform.anchoredPosition = textStartPosition;
+
+        while (totalAdvanced <= (textLength))
+        {
+            yield return new WaitForSecondsRealtime(.1f);
+            //advance text field by advanceBy in the X
+            textRextTransform.anchoredPosition = new Vector3(textStartPosition.x - totalAdvanced,
+                                                                textStartPosition.y
+                                                                );
+            totalAdvanced += advanceBy;
+
+            Debug.Log("total advanced is " + totalAdvanced);
+        }
+
+        totalAdvanced = 0;
+        yield break;
+    }
+
+    private float TextLength()
+    {
+        int stringLength = news.Length;
+        float textLengthInPx = stringLength * charLengthInPx;
+        Debug.Log("Text length in Px is " + textLengthInPx);
+
+        return textLengthInPx;
     }
 }
--- Assets/UI/UI Scripts/ScoreKeeper.cs
+++ Assets/UI/UI Scripts/ScoreKeeper.cs
@@ -8,17 +8,23 @@ public class ScoreKeeper : MonoBehaviour
 
     [SerializeField] private Text scoreText;
     [SerializeField] private int score;
+
+    private NewsReel newsReel;
+
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
+        newsReel = gameObject.GetComponent<NewsReel>();
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        //ForTesting
+    { 
+    //ForTesting
         AddToScore(1);
+
+        if(ScoreGreaterThan1mil()) return;
+        else if (ScoreGreaterThan100thou()) return;
     }
 
     //update the score and score display durring gameplay
@@ -32,5 +38,32 @@ public class ScoreKeeper : MonoBehaviour
     public int GetCurrentScore ()
     {
         return score;
+    }
+
+
+    private bool ScoreGreaterThan1mil()
+    {
+        if (score >= 1000000f)
+        {
+            newsReel.UpdateNews("Over 1 million! WOW!", true);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private bool ScoreGreaterThan100thou()
+    {
+        if (score >= 100000f)
+        {
+            newsReel.UpdateNews("You are pretty good at this.", true);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
