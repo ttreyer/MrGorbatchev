95eb6b9371eaee51d0af1fc201c70770b270e97d
Thierry Treyer <ttreyer@fb.com>
2019-11-23 02:47:16 -0800

    Fix tunnel issue

--- Assets/Scripts/TunnelHole.cs
+++ Assets/Scripts/TunnelHole.cs
@@ -19,7 +19,7 @@ public class TunnelHole : MonoBehaviour {
         ball.isKinematic = true;
 
         DOTween.Sequence()
-            .Append(ball.DOMove(_fallOffset, 1f).From(true))
+            .Append(ball.DOMove(ball.position + _fallOffset, 1f))
             .Append(ball.DOMove(_exitPosition.position + _fallOffset, 2f))
             .Append(ball.DOMove(_exitPosition.position, 1f).SetEase(Ease.InExpo))
             .AppendCallback(() => BallExit(ball));
