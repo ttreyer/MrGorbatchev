f7f353ca85c5adb8459a91706bb8d5ce4b9f337c
Thierry Treyer <ttreyer@fb.com>
2019-11-23 03:56:41 -0800

    More flexible music manager

--- Assets/UI/UI Scripts/MusicManager.cs
+++ Assets/UI/UI Scripts/MusicManager.cs
@@ -2,27 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicManager : MonoBehaviour
-{
+[System.Serializable]
+public struct MusicStage {
+    public AudioClip _intro, _loop;
+    public float _offset;
+}
+
+public class MusicManager : MonoBehaviour {
     public static MusicManager instance;
 
-    [SerializeField] private AudioClip[] musicClips;
-    [SerializeField] private AudioClip[] soundEffects;
+    public MusicStage[] musicClips;
+    public AudioClip[] soundEffects;
     private AudioSource myAudioSource;
-    [SerializeField] private bool musicIsPlaying = false;
+    public bool musicIsPlaying = false;
 
-    [SerializeField] private int currentClip =0;
+    public int currentClip = 0;
 
 
-    private void Awake()
-    {
+    private void Awake() {
 
-        if (instance == null)
-        {
+        if (instance == null) {
             instance = this;
-        }
-        else
-        {
+        } else {
             Destroy(gameObject);
         }
 
@@ -30,22 +31,18 @@ public class MusicManager : MonoBehaviour
 
         myAudioSource = gameObject.GetComponent<AudioSource>();
     }
-    
-    void Start()
-    {
+
+    void Start() {
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
     }
 
     //plays a sound effect at specified vector3 location
-    public void SpawnSoundEffect(Vector3 location, int clipIndex)
-    {
+    public void SpawnSoundEffect(Vector3 location, int clipIndex) {
         //check if the clip can exist in the array
-        if (clipIndex > soundEffects.Length)
-        {
+        if (clipIndex > soundEffects.Length) {
             Debug.Log("Clip " + clipIndex + " does not exist in the soundEffects Array.");
             return;
         }
@@ -55,11 +52,9 @@ public class MusicManager : MonoBehaviour
     }
 
     //plays a sound effect from the camera
-    public void SpawnSoundEffect(int clipIndex)
-    {
+    public void SpawnSoundEffect(int clipIndex) {
         //check if the clip can exist in the array
-        if (clipIndex > soundEffects.Length)
-        {
+        if (clipIndex > soundEffects.Length) {
             Debug.Log("Clip " + clipIndex + " does not exist in the soundEffects Array.");
             return;
         }
@@ -68,29 +63,29 @@ public class MusicManager : MonoBehaviour
         AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position);
     }
 
-    public void PlayMusic(int clipIndex)
-    {
+    public void PlayMusic(int clipIndex) {
         //check if the clip can exist in the array
-        if (clipIndex > musicClips.Length)
-        { Debug.Log("Clip " + clipIndex + " does not exist in the musicClips Array.");
+        if (clipIndex > musicClips.Length) {
+            Debug.Log("Clip " + clipIndex + " does not exist in the musicClips Array.");
             return;
         }
 
         if (currentClip == clipIndex && musicIsPlaying) return;
 
-        else if(musicIsPlaying)
-            { StopTheMusic(); }
+        else if (musicIsPlaying) { StopTheMusic(); }
 
         //assign and start the new clip
+        MusicStage stage = musicClips[currentClip];
+
         currentClip = clipIndex;
-        myAudioSource.clip = musicClips[currentClip];
+        myAudioSource.clip = stage._loop;
         myAudioSource.loop = true;
-        myAudioSource.Play();
+        myAudioSource.PlayOneShot(stage._intro);
+        myAudioSource.PlayDelayed(stage._intro.length + stage._offset);
         musicIsPlaying = true;
     }
 
-    public void StopTheMusic()
-    {
+    public void StopTheMusic() {
         myAudioSource.Stop();
         musicIsPlaying = false;
     }
