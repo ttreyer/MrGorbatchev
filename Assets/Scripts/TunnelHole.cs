318844e3123e2af0e6976540d1bc72b9ee98d2d9
Thierry Treyer <ttreyer@fb.com>
2019-11-23 00:24:45 -0800

    Add Tunnel behavior

---
+++ Assets/Scripts/TunnelHole.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class TunnelHole : MonoBehaviour {
+    public Transform _exitPosition;
+    public float _exitSpeed;
+    public Vector3 _fallOffset = new Vector3(0f, 0f, 2f);
+
+    private void OnTriggerEnter(Collider other) {
+        Rigidbody ball = other.attachedRigidbody;
+        Vector3 ballPos = ball.transform.position;
+
+        // Prevent the ball from triggering this at exit
+        if (ball.isKinematic)
+            return;
+
+        ball.isKinematic = true;
+
+        DOTween.Sequence()
+            .Append(ball.DOMove(ballPos + _fallOffset, 1f))
+            .Append(ball.DOMove(_exitPosition.position + _fallOffset, 2f))
+            .Append(ball.DOMove(_exitPosition.position, 1f).SetEase(Ease.InExpo))
+            .AppendCallback(() => BallExit(ball));
+    }
+
+    private void BallExit(Rigidbody ball) {
+        ball.isKinematic = false;
+        ball.velocity = _exitPosition.forward * _exitSpeed;
+    }
+}
