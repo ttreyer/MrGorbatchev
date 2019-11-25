40d928b38ebe274c699c16d1c073e7aeddb5f80e
Thierry Treyer <ttreyer@fb.com>
2019-11-24 16:31:37 -0800

    Faster tunnels

--- Assets/Scripts/TunnelHole.cs
+++ Assets/Scripts/TunnelHole.cs
@@ -9,6 +9,8 @@ public class TunnelHole : MonoBehaviour {
     public float _exitSpeed;
     public Vector3 _fallOffset = new Vector3(0f, 0f, 2f);
 
+    public float _fallTime = .5f, _moveTime = .5f;
+
     private void OnTriggerEnter(Collider other) {
         Rigidbody ball = other.attachedRigidbody;
 
@@ -16,17 +18,30 @@ public class TunnelHole : MonoBehaviour {
         if (ball.isKinematic)
             return;
 
-        ball.isKinematic = true;
+        BallEnter(ball);
 
         DOTween.Sequence()
-            .Append(ball.DOMove(ball.position + _fallOffset, 1f))
-            .Append(ball.DOMove(_exitPosition.position + _fallOffset, 1f))
-            .Append(ball.DOMove(_exitPosition.position, 1f).SetEase(Ease.InExpo))
+            .Append(ball.DOMove(ball.position + _fallOffset, _fallTime))
+            .Append(ball.DOMove(_exitPosition.position + _fallOffset, _moveTime))
+            .Append(ball.DOMove(_exitPosition.position, _fallTime).SetEase(Ease.InExpo))
             .AppendCallback(() => BallExit(ball));
+    }
+
+    private void BallEnter(Rigidbody ball) {
+        ball.velocity = Vector3.zero;
+        ball.interpolation = RigidbodyInterpolation.Extrapolate;
+        ball.isKinematic = true;
+
+        foreach (var col in ball.GetComponents<Collider>())
+            col.enabled = false;
     }
 
     private void BallExit(Rigidbody ball) {
         ball.isKinematic = false;
+        ball.interpolation = RigidbodyInterpolation.Interpolate;
         ball.velocity = _exitPosition.forward * _exitSpeed;
+
+        foreach (var col in ball.GetComponents<Collider>())
+            col.enabled = true;
     }
 }
